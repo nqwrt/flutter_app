329555b88433044597d57c6ad797ae09e47f0634
0216tw <xodn__love@naver.com>
2026-05-07 14:17:59 +0900

    연습문제 완성

--- lib/main.dart
+++ lib/main.dart
@@ -13,45 +13,42 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Column(
           children: [
-            // 위 영역
+
             Expanded(
-              flex: 3,
-              child: Container(
-                color: Colors.blue[100],
-              ),
+                flex: 3,
+                child: Container(
+                  color: Colors.blue[100],
+                )
             ),
-
-            // 아래 영역
             Expanded(
-              flex: 1,
-              child: Container(
-                color: Colors.orange[100],
+                flex: 1,
+                child: Container(
+                  color: Colors.orange[100],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    // crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Icon(
+                        Icons.settings,
+                        size: 40,
+                        color: Colors.blue,
+                      ),
+                      Icon(
+                        Icons.person,
+                        size: 40,
+                        color: Colors.red,
+                      ),
+                    ],
+                  ),
+                )
+            )
 
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.end,
 
-                  children: const [
-                    Icon(
-                      Icons.settings,
-                      size: 40,
-                      color: Colors.blue,
-                    ),
-
-                    //SizedBox(height: 10),
-
-                    Icon(
-                      Icons.person,
-                      size: 40,
-                      color: Colors.red,
-                    ),
-                  ],
-                ),
-              ),
-            ),
           ],
         ),
       ),
     );
   }
 }
+
+
