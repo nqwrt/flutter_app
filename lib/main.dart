b9ff214bfecc19e1e095ba4995fb922ecea58ed7
0216tw <xodn__love@naver.com>
2026-05-07 16:30:42 +0900

    연습문제 완성

--- lib/main.dart
+++ lib/main.dart
@@ -13,28 +13,29 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
 
         appBar: AppBar(
-          title: const Text('2 x 2 정사각형'),
+          title: const Text('마진과 패딩'),
           backgroundColor: Colors.blue,
         ),
 
         body: Column(
           children: [
-            Expanded(
-                child: Row(
-                  children: [
-                    Expanded(child: Container(color: Colors.red,)),
-                    Expanded(child: Container(color: Colors.green,)),
-                  ],
-                )
-            ),
-            Expanded(
-                child: Row(
-                  children: [
-                    Expanded(child: Container(color: Colors.blue,)),
-                    Expanded(child: Container(color: Colors.orange,)),
-                  ],
-                )
-            ),
+            Container(
+              width: 150,
+              height: 150,
+              margin: EdgeInsets.only(
+                  top:20,
+                  left:10,
+                  right: 10,
+                  bottom: 5),
+              padding: EdgeInsets.all(30) ,
+              color: Colors.red,
+              child: Center(
+                child: Text(
+                  "마진공부",
+                  style: TextStyle(color: Colors.white,fontSize: 20),
+                ),
+              ),
+            )
           ],
         ),
       ),
